aad115c619bbba8b8ff46f0ddcaa1bd3ac1a44f7
Tanaka Rin <47429850+s-a-d-b-o-i@users.noreply.github.com>
2019-11-12 23:21:58 -0700

    yeet

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ client.on("message", async message => {
 
     if(message.author.bot) return;
     if(!message.guild) return;
-    if(!message.content.startsWith(prefix)){ return; })
+    if(!message.content.startsWith(prefix)){ return; }
     if(!message.member) message.member = await message.guild.fetchMember(message);
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
